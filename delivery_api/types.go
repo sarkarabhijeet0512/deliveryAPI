ad9be80094c789b1901a2127b41dcdc08566a659
Abhijeet Sarkar <53407084+sarkarabhijeet0512@users.noreply.github.com>
2021-04-30 02:31:49 +0530

    Update types.go

--- delivery_api/types.go
+++ delivery_api/types.go
@@ -6,23 +6,26 @@ type Order struct {
 	// gorm.Model
 	OrderID      uint       `json:"orderId" gorm:"primary_key"`
 	CustomerName string     `json:"customerName"`
+	MobileNumber string     `json:"MobileNumber"`
 	OrderedAt    time.Time  `json:"orderedAt"`
 	Items        []Item     `json:"items" gorm:"foreignkey:OrderID"`
 	Delivery     []Delivery `json:"delivery" gorm:"foreignkey:OrderID"`
 }
 type Item struct {
 	// gorm.Model
-	LineItemID  uint   `json:"lineItemId" gorm:"primary_key"`
+	ItemID      uint   `json:"ItemId" gorm:"primary_key"`
 	ItemCode    string `json:"itemCode"`
 	Description string `json:"description"`
 	Quantity    uint   `json:"quantity"`
 	OrderID     uint   `json:"-"`
 }
 type Delivery struct {
-	DeliveryID uint `json:"deliveryId" gorm:"primary_key"`
-	Location   uint `json:"location"`
-	StatusID   uint `json:"status_id" gorm:"foreignkey:StatusID"`
-	OrderID    uint `json:"-"`
+	DeliveryID uint      `json:"deliveryId" gorm:"primary_key"`
+	Location   uint      `json:"location"`
+	StatusID   uint      `json:"status_id" gorm:"foreignkey:StatusID"`
+	OrderID    uint      `json:"-"`
+	Otp        string    `json:"otp"`
+	CreatedAt  time.Time `json:"time"`
 }
 type Status struct {
 	StatusID  uint      `json:"statusId" gorm:"primary_key"`
@@ -30,16 +33,25 @@ type Status struct {
 	CreatedAt time.Time `json:"createdat"`
 }
 type Demo struct {
+	OrderID      uint      `json:"order_id"`
 	CustomerName string    `json:"customerName"`
+	MobileNumber string    `json:"mobileNumber"`
 	OrderedAt    time.Time `json:"orderedAt"`
-	LineItemID   uint      `json:"lineItemId" gorm:"primary_key"`
+	ItemID       uint      `json:"ItemId" gorm:"primary_key"`
 	ItemCode     string    `json:"itemCode"`
 	Description  string    `json:"description"`
 	Quantity     uint      `json:"quantity"`
 	DeliveryID   uint      `json:"deliveryId" gorm:"primary_key"`
 	Location     uint      `json:"location"`
 	StatusID     uint      `json:"status_id" gorm:"foreignkey:StatusID"`
-	OrderID      uint      `json:"-"`
 	Value        string    `json:"value"`
 	CreatedAt    time.Time `json:"createdat"`
+}
+type DeliveryUpdate struct {
+	DeliveryID   uint   `json:"deliveryId" gorm:"primary_key"`
+	Location     uint   `json:"location"`
+	OrderID      uint   `json:"order_id"`
+	MobileNumber uint   `json:"MobileNumber"`
+	StatusID     uint   `json:"status_id"`
+	Otp          string `json:"otp"`
 }
