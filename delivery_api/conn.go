c8990cd45c550c79526b9b7607df53ec4ffcf30e
Abhijeet Sarkar <53407084+sarkarabhijeet0512@users.noreply.github.com>
2021-04-29 18:26:01 +0530

    Add files via upload

---
+++ delivery_api/conn.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+func InitDB() {
+	var err error
+	dataSourceName := "root:@tcp(localhost:3306)/?parseTime=True"
+	db, err = gorm.Open("mysql", dataSourceName)
+	// On the log mod for sql queries in console
+	// db.LogMode(true)
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to connect database")
+	}
+	db.Exec("CREATE DATABASE orders")
+	db.Exec("USE orders")
+
+	// Migration to create tables for Order and Item schema
+	db.AutoMigrate(&Order{}, &Item{}, &Delivery{}, &Status{})
+}
